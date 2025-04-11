eb05bc0011aa7ddee2a61fad2098a2b46d2fb698
visionariosevenn <visionario.sevenn@gmail.com>
2025-04-11 14:43:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function atualizarRTP() {
-  const rtpSlots = Math.floor(Math.random() * (98 - 94 + 1) + 94);
-  document.getElementById("slots-rtp").innerText = `RTP: ${rtpSlots}%`;
+  // Simulando atualização de RTPs (no futuro dá pra usar scraping da VenuzBet)
+  const rtp = Math.floor(Math.random() * 5 + 94); // Simula de 94 a 99%
+  document.getElementById("rtp-slots").innerText = rtp;
 }
