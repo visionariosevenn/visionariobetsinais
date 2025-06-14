21112e9462cc72ad73310b7514a42a7a683f7478
visionariosevenn <visionario.sevenn@gmail.com>
2025-06-14 17:20:25 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function atualizarRTP() {
-  // Simulando atualização de RTPs (no futuro dá pra usar scraping da VenuzBet)
-  const rtp = Math.floor(Math.random() * 5 + 94); // Simula de 94 a 99%
-  document.getElementById("rtp-slots").innerText = rtp;
-}
